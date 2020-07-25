a480ce8a6c02b2f10f12abbf20ac3b54d4ce6183
kevinconvery <kevinconvery@gmail.com>
2020-07-25 18:25:34 -0400

    modify data sources for testing

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,6 @@
 export const taskList = [
   {
-    day: 1,
+    day: 0,
     week: 1,
     start: 12,
     end: 15,
@@ -10,7 +10,7 @@ export const taskList = [
     driver_id: 2
   },
   {
-    day: 1,
+    day: 0,
     week: 1,
     start: 9,
     end: 10,
@@ -20,7 +20,7 @@ export const taskList = [
     driver_id: 3
   },
   {
-    day: 3,
+    day: 2,
     week: 1,
     start: 17,
     end: 18,
@@ -52,8 +52,8 @@ export const taskList = [
 ]
 
 export const scheduleTimes = ['12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', 
-  '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 AM', '1 PM', '2 PM', 
-  '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM', '12 PM'
+  '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', 
+  '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM'
 ]
 
 export const weekdays = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
