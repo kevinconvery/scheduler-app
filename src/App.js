cccb13bdd8575fc5651f8e6c880767ff24c57ea8
kevinconvery <kevinconvery@gmail.com>
2020-07-25 12:42:48 -0400

    add hooks to handle state

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, { useState } from 'react'
 import Header from './components/Header/Header'
 
 function App() {
+  const [currentWeek, setCurrentWeek] = useState(1)
+  const [currentDriver, setCurrentDriver] = useState(0)
+  const [scheduleInterval, setScheduleInterval] = useState(0)
+
   return (
     <div className="App">
-      <Header />
+      <Header 
+        week={currentWeek}
+        updateWeek={setCurrentWeek}
+        updateDriver={setCurrentDriver}
+        updateScheduleInterval={setScheduleInterval} 
+      />
     </div>
   )
 }
