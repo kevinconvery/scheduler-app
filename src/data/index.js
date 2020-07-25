2047535dd86954342dc34e00d9e0e1bf1511de43
kevinconvery <kevinconvery@gmail.com>
2020-07-25 15:05:08 -0400

    add scheduleTimes to data/index.js

--- src/data/index.js
+++ src/data/index.js
@@ -50,3 +50,81 @@ export const taskList = [
     driver_id: 2
   }
 ]
+
+export const scheduleTimes = [
+  { 
+    key: 0, value: '12 AM'
+  },
+  {
+    key: 1, value: '1 AM',
+  },
+  {
+    key: 2, value: '2 AM',
+  },
+  {
+    key: 3, value: '3 AM',
+  },
+  {
+    key: 4, value: '4 AM',
+  },
+  {
+    key: 5, value: '5 AM',
+  },
+  {
+    key: 6, value: '6 AM',
+  },
+  {
+    key: 7, value: '7 AM',
+  },
+  {
+    key: 8, value: '8 AM',
+  },
+  {
+    key: 9, value: '9 AM',
+  },
+  {
+    key: 10, value: '10 AM',
+  },
+  {
+    key: 11, value: '11 AM',
+  },
+  {
+    key: 12, value: '12 PM',
+  },
+  {
+    key: 13, value: '1 PM',
+  },
+  {
+    key: 14, value: '2 PM',
+  },
+  {
+    key: 15, value: '3 PM',
+  },
+  {
+    key: 16, value: '4 PM',
+  },
+  {
+    key: 17, value: '5 PM',
+  },
+  {
+    key: 18, value: '6 PM',
+  },
+  {
+    key: 19, value: '7 PM',
+  },
+  {
+    key: 20, value: '8 PM',
+  },
+  {
+    key: 21, value: '9 PM',
+  },
+  {
+    key: 22, value: '10 PM',
+  },
+  {
+    key: 23, value: '11 PM',
+  },
+  {
+    key: 24, value: '12 AM'
+  }
+]
