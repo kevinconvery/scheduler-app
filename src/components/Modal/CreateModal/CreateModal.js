b76dba562be7812f78df52804c444908a5faf2da
kevinconvery <kevinconvery@gmail.com>
2020-07-30 16:24:05 -0400

    add conditional rendering to EditModal and CreateModal for buttons in the form

--- src/components/Modal/CreateModal/CreateModal.js
+++ src/components/Modal/CreateModal/CreateModal.js
@@ -29,153 +29,155 @@ const CreateModal = props => {
 
   return ( 
     <div className="CreateModal">
-      {errorModalVisible || (<form
-        className="create-item-form" 
-        onSubmit={handleCreateItemSubmit}
-      >
-        <div className="form-field">
-          <label htmlFor="driver">Driver:</label>
-          <select 
-            name="driver"
-            value={creationTask.driver_id}
-            onChange={e => setCreationTask({...creationTask, driver_id: parseInt(e.target.value)})}
-          >
-            <option value="">-- Select a driver --</option>
-            {drivers.map(driver => (
-              <option key={driver} value={drivers.indexOf(driver) + 1}>{driver}</option>
-            ))}
-          </select>
-        </div>
-        <div className="form-field">
-        <label htmlFor="week">Week of task:</label>
-          <select 
-            name="week"
-            value={creationTask.week}
-            onChange={e => setCreationTask({...creationTask, week: parseInt(e.target.value)})}
-          >
-            {Array(52)
-              .fill()
-              .map((key, value) => (
+      {errorModalVisible || (
+        <form
+          className="create-item-form" 
+          onSubmit={handleCreateItemSubmit}
+        >
+          <div className="form-field">
+            <label htmlFor="driver">Driver:</label>
+            <select 
+              name="driver"
+              value={creationTask.driver_id}
+              onChange={e => setCreationTask({...creationTask, driver_id: parseInt(e.target.value)})}
+            >
+              <option value="">-- Select a driver --</option>
+              {drivers.map(driver => (
+                <option key={driver} value={drivers.indexOf(driver) + 1}>{driver}</option>
+              ))}
+            </select>
+          </div>
+          <div className="form-field">
+          <label htmlFor="week">Week of task:</label>
+            <select 
+              name="week"
+              value={creationTask.week}
+              onChange={e => setCreationTask({...creationTask, week: parseInt(e.target.value)})}
+            >
+              {Array(52)
+                .fill()
+                .map((key, value) => (
+                  <option 
+                    key={`week-${value}`} 
+                    value={(value + 1)}
+                  >
+                    {value + 1}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="form-field">
+            <label htmlFor="task-day">Day of task:</label>
+            <select 
+              name="task-day"
+              value={creationTask.day}
+              onChange={e => setCreationTask({...creationTask, day: parseInt(e.target.value)})}
+            >
+              {weekdays.map(weekday => (
+                <option value={weekdays.indexOf(weekday)} key={weekday}>{weekday}</option>
+              ))}
+            </select>
+          </div>
+
+          <div className="form-field">
+            <label htmlFor="start-time">Start Time:</label>
+            <select 
+              name="start-time"
+              value={creationTask.start}  
+              onChange={e => setCreationTask({...creationTask, start: parseInt(e.target.value)})}
+            >
+              {scheduleTimes
+                .map(time => (
+                  <option
+                    key={`start-time-${time}`} 
+                    name={time} 
+                    value={scheduleTimes.indexOf(time)}
+                  >
+                    {scheduleTimes[scheduleTimes.indexOf(time)]}
+                  </option>
+                )
+              )}
+            </select>
+          </div>
+          <div className="form-field">
+            <label htmlFor="end-time">End Time:</label>
+            <select 
+              name="end-time"
+              onChange={e => setCreationTask({...creationTask, end: parseInt(e.target.value)})}
+              value={creationTask.end}
+            >
+              {scheduleTimes
+                .slice(creationTask.start + 1)
+                .map(time => (
+                  <option
+                    key={`end-time-${time}`}
+                    name={time}
+                    value={scheduleTimes.indexOf(time)}
+                  >
+                    {scheduleTimes[scheduleTimes.indexOf(time)]}
+                  </option>              
+                )
+              )}
+            </select>
+          </div>
+          <div className="form-field">
+            <label htmlFor="task-type">What type of task is this?</label>
+            <select 
+              name="task-type"
+              onChange={e => setCreationTask({...creationTask, type: e.target.value })}
+              value={creationTask.type}
+            >
+              {taskTypes.map(type => (
                 <option 
-                  key={`week-${value}`} 
-                  value={(value + 1)}
+                  key={`task-type-${type}`}
+                  name={type}
+                  value={type}
                 >
-                  {value + 1}
+                  {type}
                 </option>
               ))}
-          </select>
-        </div>
-        <div className="form-field">
-          <label htmlFor="task-day">Day of task:</label>
-          <select 
-            name="task-day"
-            value={creationTask.day}
-            onChange={e => setCreationTask({...creationTask, day: parseInt(e.target.value)})}
-          >
-            {weekdays.map(weekday => (
-              <option value={weekdays.indexOf(weekday)} key={weekday}>{weekday}</option>
-            ))}
-          </select>
-        </div>
-
-        <div className="form-field">
-          <label htmlFor="start-time">Start Time:</label>
-          <select 
-            name="start-time"
-            value={creationTask.start}  
-            onChange={e => setCreationTask({...creationTask, start: parseInt(e.target.value)})}
-          >
-            {scheduleTimes
-              .map(time => (
-                <option
-                  key={`start-time-${time}`} 
-                  name={time} 
-                  value={scheduleTimes.indexOf(time)}
-                >
-                  {scheduleTimes[scheduleTimes.indexOf(time)]}
-                </option>
-              )
-            )}
-          </select>
-        </div>
-        <div className="form-field">
-          <label htmlFor="end-time">End Time:</label>
-          <select 
-            name="end-time"
-            onChange={e => setCreationTask({...creationTask, end: parseInt(e.target.value)})}
-            value={creationTask.end}
-          >
-            {scheduleTimes
-              .slice(creationTask.start + 1)
-              .map(time => (
-                <option
-                  key={`end-time-${time}`}
-                  name={time}
-                  value={scheduleTimes.indexOf(time)}
-                >
-                  {scheduleTimes[scheduleTimes.indexOf(time)]}
-                </option>              
-              )
-            )}
-          </select>
-        </div>
-        <div className="form-field">
-          <label htmlFor="task-type">What type of task is this?</label>
-          <select 
-            name="task-type"
-            onChange={e => setCreationTask({...creationTask, type: e.target.value })}
-            value={creationTask.type}
-          >
-            {taskTypes.map(type => (
-              <option 
-                key={`task-type-${type}`}
-                name={type}
-                value={type}
-              >
-                {type}
-              </option>
-            ))}
-          </select> 
-        </div>
-        <div className="form-field">
-          <label htmlFor="task-description">
-            Please describe the task briefly:
-          </label>
-          <input 
-            type="text"
-            className="text-description-field"
-            value={creationTask.description}
-            onChange={e => setCreationTask({...creationTask, description: e.target.value })}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="task-location">
-            Where is the driver going?
-          </label>
-          <input
-            type="text"
-            className="text-location-field"
-            value={creationTask.location}
-            onChange={e => setCreationTask({...creationTask, location: e.target.value })}
-          />
-        </div>
-        {errorModalVisible || (
-          <div className="modal-button-section">
-            <button 
-              type="submit"
-              className="modal-btn create-task-button"
-            >
-              Create Task
-            </button>
-            <button
-              className="modal-btn toggle-modal-view-button"
-              onClick={() => toggleModalView("CREATE")}
-            >
-              Toggle View
-            </button>
+            </select> 
           </div>
-        )}
-      </form>)}
+          <div className="form-field">
+            <label htmlFor="task-description">
+              Please describe the task briefly:
+            </label>
+            <input 
+              type="text"
+              className="text-description-field"
+              value={creationTask.description}
+              onChange={e => setCreationTask({...creationTask, description: e.target.value })}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="task-location">
+              Where is the driver going?
+            </label>
+            <input
+              type="text"
+              className="text-location-field"
+              value={creationTask.location}
+              onChange={e => setCreationTask({...creationTask, location: e.target.value })}
+            />
+          </div>
+          {errorModalVisible || (
+            <div className="modal-button-section">
+              <button 
+                type="submit"
+                className="modal-btn create-task-button"
+              >
+                Create Task
+              </button>
+              <button
+                className="modal-btn toggle-modal-view-button"
+                onClick={() => toggleModalView("CREATE")}
+              >
+                Toggle View
+              </button>
+            </div>
+          )}
+        </form>
+      )}
       {errorModalVisible && (
         <div className="error-modal">
           <div className="error-message">
--- src/components/Modal/EditModal/EditModal.js
+++ src/components/Modal/EditModal/EditModal.js
@@ -35,151 +35,153 @@ const EditModal = props => {
 
   return (
     <div className="EditModal">
-      <form
-        className="edit-item-form" 
-        onSubmit={handleUpdateItemSubmit}
-      >
-        <div className="form-field">
-          <label htmlFor="driver">Driver:</label>
-          <select 
-            name="driver"
-            value={editTask.driver_id}
-            onChange={e => setEditTask({...editTask, driver_id: parseInt(e.target.value)})}
-          >
-            {drivers.map(driver => (
-              <option key={driver} value={drivers.indexOf(driver) + 1}>{driver}</option>
-            ))}
-          </select>
-        </div>
-        <div className="form-field">
-          <label htmlFor="week">Week of task:</label>
-          <select 
-            name="week"
-            value={editTask.week}
-            onChange={e => setEditTask({...editTask, week: parseInt(e.target.value)})}
-          >
-            {Array(52)
-              .fill()
-              .map((key, value) => (
+      {errorModalVisible || (
+        <form
+          className="edit-item-form" 
+          onSubmit={handleUpdateItemSubmit}
+        >
+          <div className="form-field">
+            <label htmlFor="driver">Driver:</label>
+            <select 
+              name="driver"
+              value={editTask.driver_id}
+              onChange={e => setEditTask({...editTask, driver_id: parseInt(e.target.value)})}
+            >
+              {drivers.map(driver => (
+                <option key={driver} value={drivers.indexOf(driver) + 1}>{driver}</option>
+              ))}
+            </select>
+          </div>
+          <div className="form-field">
+            <label htmlFor="week">Week of task:</label>
+            <select 
+              name="week"
+              value={editTask.week}
+              onChange={e => setEditTask({...editTask, week: parseInt(e.target.value)})}
+            >
+              {Array(52)
+                .fill()
+                .map((key, value) => (
+                  <option 
+                    key={`week-${value}`} 
+                    value={(value + 1)}
+                  >
+                    {value + 1}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="form-field">
+            <label htmlFor="task-day">Day of task:</label>
+            <select 
+              name="task-day"
+              value={editTask.day}
+              onChange={e => setEditTask({...editTask, day: parseInt(e.target.value)})}
+            >
+              {weekdays.map(weekday => (
+                <option value={weekdays.indexOf(weekday)} key={weekday}>{weekday}</option>
+              ))}
+            </select>
+          </div>
+
+          <div className="form-field">
+            <label htmlFor="start-time">Start Time:</label>
+            <select 
+              name="start-time"
+              value={editTask.start}  
+              onChange={e => setEditTask({...editTask, start: parseInt(e.target.value)})}
+            >
+              {scheduleTimes
+                .map(time => (
+                  <option
+                    key={`start-time-${time}`} 
+                    name={time} 
+                    value={scheduleTimes.indexOf(time)}
+                  >
+                    {scheduleTimes[scheduleTimes.indexOf(time)]}
+                  </option>
+                )
+              )}
+            </select>
+          </div>
+          <div className="form-field">
+            <label htmlFor="end-time">End Time:</label>
+            <select 
+              name="end-time"
+              value={editTask.end}
+              onChange={e => setEditTask({...editTask, end: parseInt(e.target.value)})}
+            >
+              {scheduleTimes
+                .slice(editTask.start)
+                .map(time => (
+                  <option
+                    key={`end-time-${time}`}
+                    name={time}
+                    value={scheduleTimes.indexOf(time)}
+                  >
+                    {scheduleTimes[scheduleTimes.indexOf(time)]}
+                  </option>              
+                )
+              )}
+            </select>
+          </div>
+          <div className="form-field">
+            <label htmlFor="task-type">What type of task is this?</label>
+            <select 
+              name="task-type"
+              value={editTask.type}
+              onChange={e => setEditTask({...editTask, type: e.target.value})}
+            >
+              {taskTypes.map(type => (
                 <option 
-                  key={`week-${value}`} 
-                  value={(value + 1)}
+                  key={`task-type-${type}`}
+                  name={type}
+                  value={type}
                 >
-                  {value + 1}
+                  {type}
                 </option>
               ))}
-          </select>
-        </div>
-        <div className="form-field">
-          <label htmlFor="task-day">Day of task:</label>
-          <select 
-            name="task-day"
-            value={editTask.day}
-            onChange={e => setEditTask({...editTask, day: parseInt(e.target.value)})}
-          >
-            {weekdays.map(weekday => (
-              <option value={weekdays.indexOf(weekday)} key={weekday}>{weekday}</option>
-            ))}
-          </select>
-        </div>
-
-        <div className="form-field">
-          <label htmlFor="start-time">Start Time:</label>
-          <select 
-            name="start-time"
-            value={editTask.start}  
-            onChange={e => setEditTask({...editTask, start: parseInt(e.target.value)})}
-          >
-            {scheduleTimes
-              .map(time => (
-                <option
-                  key={`start-time-${time}`} 
-                  name={time} 
-                  value={scheduleTimes.indexOf(time)}
-                >
-                  {scheduleTimes[scheduleTimes.indexOf(time)]}
-                </option>
-              )
-            )}
-          </select>
-        </div>
-        <div className="form-field">
-          <label htmlFor="end-time">End Time:</label>
-          <select 
-            name="end-time"
-            value={editTask.end}
-            onChange={e => setEditTask({...editTask, end: parseInt(e.target.value)})}
-          >
-            {scheduleTimes
-              .slice(editTask.start)
-              .map(time => (
-                <option
-                  key={`end-time-${time}`}
-                  name={time}
-                  value={scheduleTimes.indexOf(time)}
-                >
-                  {scheduleTimes[scheduleTimes.indexOf(time)]}
-                </option>              
-              )
-            )}
-          </select>
-        </div>
-        <div className="form-field">
-          <label htmlFor="task-type">What type of task is this?</label>
-          <select 
-            name="task-type"
-            value={editTask.type}
-            onChange={e => setEditTask({...editTask, type: e.target.value})}
-          >
-            {taskTypes.map(type => (
-              <option 
-                key={`task-type-${type}`}
-                name={type}
-                value={type}
-              >
-                {type}
-              </option>
-            ))}
-          </select> 
-        </div>
-        <div className="form-field">
-          <label htmlFor="task-description">
-            Please describe the task briefly:
-          </label>
-          <input 
-            type="text"
-            className="text-description-field"
-            value={editTask.description}
-            onChange={e => setEditTask({...editTask, description: e.target.value})}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="task-location">
-            Where is the driver going?
-          </label>
-          <input
-            type="text"
-            className="text-location-field"
-            value={editTask.location}
-            onChange={e => setEditTask({...editTask, location: e.target.value})}
-          />
-        </div>
-        {errorModalVisible || (
-          <div className="modal-button-section">
-            <button 
-              type="submit"
-              className="modal-btn update-task-button"
-            >
-              Update Task
-            </button>
-            <button
-              className="modal-btn delete-task-button"
-              onClick={e => handleDeleteTask(e)}>
-              Delete Task
-            </button>
+            </select> 
           </div>
-        )}
-      </form>
+          <div className="form-field">
+            <label htmlFor="task-description">
+              Please describe the task briefly:
+            </label>
+            <input 
+              type="text"
+              className="text-description-field"
+              value={editTask.description}
+              onChange={e => setEditTask({...editTask, description: e.target.value})}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="task-location">
+              Where is the driver going?
+            </label>
+            <input
+              type="text"
+              className="text-location-field"
+              value={editTask.location}
+              onChange={e => setEditTask({...editTask, location: e.target.value})}
+            />
+          </div>
+          {errorModalVisible || (
+            <div className="modal-button-section">
+              <button 
+                type="submit"
+                className="modal-btn update-task-button"
+              >
+                Update Task
+              </button>
+              <button
+                className="modal-btn delete-task-button"
+                onClick={e => handleDeleteTask(e)}>
+                Delete Task
+              </button>
+            </div>
+          )}
+        </form>
+      )}
       {errorModalVisible && (
         <div className="error-modal">
           <div className="error-message">
@@ -201,12 +203,14 @@ const EditModal = props => {
           </div>
         </div>
       )}
-      <button
-        className="modal-btn toggle-modal-view-button" 
-        onClick={() => toggleModalView("EDIT")}
-      >
-        Toggle View
-      </button>
+      {errorModalVisible || (
+        <button
+          className="modal-btn toggle-modal-view-button" 
+          onClick={() => toggleModalView("EDIT")}
+        >
+          Toggle View
+        </button>
+      )}
     </div>    
   )
 }
